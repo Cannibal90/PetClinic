424b4fbdf7cd6cd59bbb546b8e7a71a96db7b279
Krystian Kopeć <73670385+Cannibal90@users.noreply.github.com>
2022-07-06 10:58:20 +0200

    Initial commit

---
+++ src/test/java/com/cannibal90/petclinic/PetclinicApplicationTests.java
@@ -0,0 +1,11 @@
+package com.cannibal90.petclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetclinicApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
